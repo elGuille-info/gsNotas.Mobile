adc52d329aed67096969172d561c36fc52a781fb
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2020-12-26 21:13:02 +0100

    actualizado 26-dic-2020 2112
    con aplicación para iOS

---
+++ gsNotasNET.iOS/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("gsNotasNET.iOS")]
+[assembly: AssemblyDescription("Utilidad para tomar notas y guardarlas en el dispositivo. (revisión del 26-dic-2020)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("elGuille")]
+[assembly: AssemblyProduct("gsNotasNET.iOS")]
+[assembly: AssemblyCopyright("Copyright © Guillermo Som (elGuille), 2020")]
+[assembly: AssemblyTrademark("Creado con Xamarin.Forms y .NET Standard 2.0")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("50c7b8c9-e664-45af-b88e-0c9b8b9c1be1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.26")]
+[assembly: AssemblyFileVersion("2.0.0.26")]
+
+// Add some common permissions, these can be removed if not needed
+//[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+//[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+
