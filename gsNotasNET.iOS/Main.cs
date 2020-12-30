97376f85a8dfe7aacc7a82e53207a7d52902346d
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2020-12-30 08:11:48 +0100

    actualización 30-dic-2020 0811

--- gsNotasNET.iOS/Main.cs
+++ gsNotasNET.iOS/Main.cs
@@ -1,4 +1,5 @@
 ﻿using UIKit;
+using gsNotasNET;
 
 namespace gsNotasNET.iOS
 {
@@ -7,6 +8,8 @@ namespace gsNotasNET.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            App.AppName = "gsNotasNET.iOS";
+
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
